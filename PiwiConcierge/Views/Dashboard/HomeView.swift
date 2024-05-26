8b2c2a8a238f6660321784eb3f9ea60a2a9ea9f4
Derek <someflydays@gmail.com>
2024-05-25 19:43:31 -0700

    13 (centered product)

--- PiwiConcierge/Views/Dashboard/HomeView.swift
+++ PiwiConcierge/Views/Dashboard/HomeView.swift
@@ -12,134 +12,122 @@ struct HomeView: View {
     @State private var isDragging: Bool = false
 
     var body: some View {
-        ZStack {
-            VStack(spacing: 20) {
-                if let product = userData.currentProduct {
-                    HStack {
-                        VStack(alignment: .leading, spacing: 10) {
-                            Text(product.name)
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .multilineTextAlignment(.leading)
+        VStack(spacing: 20) {
+            if let product = userData.currentProduct {
+                HStack {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(product.name)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.leading)
+                            .padding(.top, 20) // This pads the Product title to the top
 
-                            Text(product.description)
-                                .font(.headline)
-                                .foregroundColor(.secondary)
-                                .multilineTextAlignment(.leading)
-
-                            Text("$\(product.price, specifier: "%.2f")")
-                                .font(.title2)
-                                .foregroundColor(.primary)
-                                .padding(.top, 10)
-
-                            HStack(spacing: 30) {
-                                Button(action: { handleAddToCart(product: product) }) {
-                                    Image(systemName: "cart.fill")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 40, height: 40)
-                                        .foregroundColor(.blue)
-                                }
-                                .buttonStyle(PlainButtonStyle()) // Apply a plain button style to remove default styling
-
-                                Button(action: { handleCreateCollection(product: product) }) {
-                                    Image(systemName: "plus.circle.fill")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 40, height: 40)
-                                        .foregroundColor(.green)
-                                }
-                                .buttonStyle(PlainButtonStyle()) // Apply a plain button style to remove default styling
-
-                                Button(action: {
-                                    handleSaveForLater(product: product)
-                                }) {
-                                    Image(systemName: product.isSaved ? "bookmark.fill" : "bookmark")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 40, height: 40)
-                                        .foregroundColor(.white)
-                                }
-                                .buttonStyle(PlainButtonStyle()) // Apply a plain button style to remove default styling
-                            }
+                        Text(product.description)
+                            .font(.headline)
+                            .foregroundColor(.secondary)
+                            .multilineTextAlignment(.leading)
                             .padding(.top, 10)
 
-                            Spacer()
-                        }
-                        .padding(.leading, 20)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("$\(product.price, specifier: "%.2f")")
+                            .font(.title2)
+                            .foregroundColor(.primary)
+                            .padding(.top, 10)
 
-                        InteractiveProductCard(
-                            product: product
-                        )
-                        .frame(maxWidth: .infinity)
-                        //.padding(.trailing, 20)
-                        .padding()
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .transition(.opacity.animation(.easeInOut(duration: 1.0))) // Slower fade transition
-                    }
-                } else {
-                    Text("No more recommendations")
-                        .font(.title)
-                        .foregroundColor(.secondary)
-                        .padding()
-                }
-
-                Spacer()
-
-                HStack {
-                    // The Back Button only appears at the second recommendation, and beyond
-                    if userData.currentIndex > 0 {
-                        HStack {
-                            Button(action: { userData.showPreviousProduct() }) {
-                                Image(systemName: "chevron.left")
+                        HStack(spacing: 30) {
+                            Button(action: { handleAddToCart(product: product) }) {
+                                Image(systemName: "cart.fill")
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
-                                    .frame(width: 35, height: 35)
-                                    .padding()
+                                    .frame(width: 40, height: 40)
+                                    .foregroundColor(.blue)
+                            }
+                            .buttonStyle(PlainButtonStyle())
+
+                            Button(action: { handleCreateCollection(product: product) }) {
+                                Image(systemName: "plus.circle.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40, height: 40)
+                                    .foregroundColor(.green)
+                            }
+                            .buttonStyle(PlainButtonStyle())
+
+                            Button(action: {
+                                handleSaveForLater(product: product)
+                            }) {
+                                Image(systemName: product.isSaved ? "bookmark.fill" : "bookmark")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40, height: 40)
                                     .foregroundColor(.white)
-                                    .background(Color.gray.opacity(0.5))
-                                    .clipShape(Circle())
                             }
-                            .buttonStyle(PlainButtonStyle()) // Apply a plain button style to remove default styling
-                            .padding(.leading, 20)
-                            Spacer()
+                            .buttonStyle(PlainButtonStyle())
                         }
-                    }
-                    Spacer()
-                    HStack {
+                        .padding(.top, 20) // This pads the row of buttons to the top
+
                         Spacer()
-                        Button(action: {
-                            isDragging.toggle()
-                        }) {
-                            Image(systemName: "cube")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 40, height: 40)
-                                .padding()
-                                .foregroundColor(.white)
-                        }
-                        .gesture(isDragging ? DragGesture()
-                            .onChanged { value in
-                                // Update model position based on drag
-                            }
-                            .onEnded { _ in
-                                isDragging = false
-                            }
-                                 : nil)
-                        .padding()
-                        .buttonStyle(PlainButtonStyle()) // Apply a plain button style to remove default styling
                     }
+                    .padding(.leading, 20) // This pads the entire VStack to the left
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+
+                    InteractiveProductCard(
+                        product: product
+                    )
+                    .padding(.leading, 250) // These three center the product in the right side of the window
+                    .padding(.trailing, 250)
+                    .padding(.top, 60)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .transition(.opacity.animation(.easeInOut(duration: 1.0))) // Slower fade transition
+                    
                 }
+            } else {
+                Text("No more recommendations")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                    .padding()
             }
-            .padding()
-            .gesture(
-                TapGesture(count: 3)
-                    .onEnded {
-                        userData.showNextProduct()
+
+            //Spacer() // This adds a bit of space between the upper HStack and the lower HStack
+
+            HStack {
+                // The Back Button only appears at the second recommendation, and beyond
+                if userData.currentIndex > 0 {
+                    Button(action: { userData.showPreviousProduct() }) {
+                        Image(systemName: "chevron.left")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 35, height: 35)
+                            .padding() // For some reason, this makes the circle around the button bigger
+                            .foregroundColor(.white)
+                            //.background(Color.gray.opacity(0.5))
+                            //.clipShape(Circle())
                     }
-            )
+                    .buttonStyle(PlainButtonStyle())
+                    .padding(.leading, 20) // This pads the Back button to the left
+                }
+                Spacer() // This spacer places the Cube button to the right (instead of the center)
+                Button(action: {
+                    isDragging.toggle()
+                }) {
+                    Image(systemName: "cube")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40, height: 40)
+                        .padding()
+                        .foregroundColor(.white)
+                }
+                .buttonStyle(PlainButtonStyle())
+                .padding(.trailing, 20) // This pads the Cube button to the right
+            }
         }
+        .padding() // This keeps the product title and info from being too close to the top of the screen
+        .gesture(
+            TapGesture(count: 3)
+                .onEnded {
+                    userData.showNextProduct()
+                }
+        )
     }
 
     private func handleSwipeLeft(product: Product) {
